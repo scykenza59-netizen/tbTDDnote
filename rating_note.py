5632f37399f186ed5aedcfd13c9b6558fc9a9b76
kenza39 <scykenza59@gmail.com>
2025-11-13 10:41:32 +0100

    blue  refecoring

--- rating_note.py
+++ rating_note.py
@@ -2,8 +2,8 @@ def rate_note(note: int) -> str:
     if note < 10:
         return "unsuccessful"
 
-    if note == 13 or note == 12:
-        return "good"
+    if  10 <= note < 12:
+        return "acceptable"
     if note == 14:
         return " very good"
-    return "acceptable"
+    return "good"
