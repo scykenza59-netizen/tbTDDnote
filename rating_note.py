59419f4b45481a1da086840924c90c3221489af2
kenza39 <scykenza59@gmail.com>
2025-11-13 10:00:53 +0100

    blue refectoring

--- rating_note.py
+++ rating_note.py
@@ -1,6 +1,5 @@
 def rate_note(note: int) -> str:
-    if note == 8:
-        return "unsuccessful"
-    if note == 9:
+
+    if note == 8 or note == 9:
         return "unsuccessful"
     return "acceptable"
