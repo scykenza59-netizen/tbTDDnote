1ab739957cd06088a2605178b55e6b3bd84730df
kenza39 <scykenza59@gmail.com>
2025-11-13 11:20:05 +0100

    blue refectoring

--- tests/test_rating_note.py
+++ tests/test_rating_note.py
@@ -2,29 +2,27 @@ import pytest
 
 from rating_note import rate_note
 
-def test_rate_11_returns_acceptable():
-    assert rate_note(11) == "acceptable"
 
-@pytest.mark.parametrize("note", [7, 8, 9])
-def test_rate_note_unsuccessful(note):
-    assert rate_note(note) == "unsuccessful"
+@pytest.mark.parametrize(
+    "note, expected",
+    [
+        (7, "unsuccessful"),
+        (8, "unsuccessful"),
+        (9, "unsuccessful"),
+        (10, "acceptable"),
+        (11, "acceptable"),
+        (12, "good"),
+        (13, "good"),
+        (14, " very good"),
+        (15, " very good"),
+        (16,"excellent"),
+        (17,"excellent"),
+        (18,"excellent"),
+        (19,"excellent"),
 
-def test_rate_note_13_good():
-    assert rate_note(13) == "good"
-
-def test_rate_note_12_good():
-    assert rate_note(12) == "good"
-
-def test_rate_note_14_good():
-        assert rate_note(14) == " very good"
 
 
-def test_rate_note_15_very_good():
-    assert rate_note(15) == " very good"
-
-def test_rate_note_16_exellent():
-    assert rate_note(16) ==  "excellent"
-def test_rate_note_17_exellent():
-    assert rate_note(17) ==  "excellent"
-def test_rate_note_19_exellent():
-    assert rate_note(19) ==  "excellent"
+    ]
+)
+def test_rating_note(note, expected):
+    assert rate_note(note) == expected
