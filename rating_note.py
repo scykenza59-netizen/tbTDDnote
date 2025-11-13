a187fc47aad8929d1487028521b742e7e664c36a
kenza39 <scykenza59@gmail.com>
2025-11-13 10:32:01 +0100

    green- rating (12) returns good

--- rating_note.py
+++ rating_note.py
@@ -1,6 +1,9 @@
 def rate_note(note: int) -> str:
     if note < 10:
         return "unsuccessful"
+    if note == 12:
+        return "good"
     if note == 13:
         return "good"
+
     return "acceptable"
