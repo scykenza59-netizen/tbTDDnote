12a3f8fadcd616da6afe3428a7f5087cc6e5717a
kenza39 <scykenza59@gmail.com>
2025-11-13 10:22:59 +0100

    green - rate_note(11) returns acceptable

--- tests/test_rating_note.py
+++ tests/test_rating_note.py
@@ -2,14 +2,12 @@ import pytest
 
 from rating_note import rate_note
 
-
+def test_rate_11_returns_acceptable():
+    assert rate_note(11) == "acceptable"
 
 @pytest.mark.parametrize("note", [7, 8, 9])
 def test_rate_note_unsuccessful(note):
     assert rate_note(note) == "unsuccessful"
 
-def test_rate_10_returns_acceptable():
-            assert rate_note(10) == "acceptable"
 
-def test_rate_8_returns_unsuccessful():
-    assert rate_note(8) == "unsuccessful"
+
